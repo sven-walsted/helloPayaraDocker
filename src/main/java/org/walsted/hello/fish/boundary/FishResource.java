a4c1d0747f9b828eb0f185d0d05afa3988e068e5
Sven Walsted <swalsted@gmail.com>
2020-10-08 11:34:21 -0800

    Add RESTful API and Persistance for Fish
    This commit includes adding Persistance API configuration, Stateless EJB,
    and Fish entity. README.md has curl examples to create, read, update, and
    delete Fish records.

---
+++ src/main/java/org/walsted/hello/fish/boundary/FishResource.java
@@ -0,0 +1,101 @@
+package org.walsted.hello.fish.boundary;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.Response.Status;
+
+import org.walsted.hello.fish.control.FishService;
+import org.walsted.hello.fish.entity.Fish;
+
+@Path("fish")
+public class FishResource {
+
+	@EJB
+	private FishService service;
+
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@GET
+	public JsonArray find() {
+		return newJsonArray(service.findAll());
+	}
+
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@GET
+	@Path("/{id}")
+	public JsonObject findById(@PathParam("id") long id) {
+		return buildJsonObject(service.find(id));
+	}
+
+	@POST
+	public Response create(@Valid Fish fish) {
+		service.create(fish);
+		URI newEntityUri = UriBuilder.fromResource(FishResource.class).path(fish.getId().toString()).build();
+		return Response.created(newEntityUri).build();
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{id}")
+	public Response update(@PathParam("id") long id, Fish entity) {
+		// Set ID, since you don't know how many properties are present.
+		entity.setId(id);
+		service.update(entity);
+		return Response.ok().build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	public Response deleteById(@PathParam("id") long id) {
+		try {
+			service.delete(id);
+			return Response.status(Status.OK).build();
+		} catch (Exception e) {
+			System.err.println("Delete: " + e.getMessage());
+			return Response.status(Status.NOT_FOUND).build();
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	//
+
+	private JsonArray newJsonArray(List<Fish> list) {
+		JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+		for (Fish entity : list) {
+			try {
+				arrayBuilder.add(buildJsonObject(entity));
+			} catch (RuntimeException e) {
+				System.err.println("Could not build JsonObject for: " + entity);
+			}
+		}
+		return arrayBuilder.build();
+	}
+
+	private JsonObject buildJsonObject(Fish fish) {
+		JsonObjectBuilder builder = Json.createObjectBuilder().add("id", fish.getId())
+				.add("className", fish.getClassName()).add("orderName", fish.getOrderName())
+				.add("familyName", fish.getFamilyName()).add("genusName", fish.getGenusName())
+				.add("speciesName", fish.getSpeciesName()).add("commonName", fish.getCommonName());
+
+		return builder.build();
+	}
+
+}
--- testDeployOnDocker.sh
+++ testDeployOnDocker.sh
@@ -5,23 +5,24 @@
 # directory, and mounts that directory to the running Docker 
 # instance.
 #
+# Note: this Test-instance uses bundled H2 database.
+#
 # The Docker instance exposes ports 4848, 8080, and 8181 to the
 # localhost.
 ######################################################################
 # Environment setup
 IMAGE_NAME=payara/server-full:5.193
-INSTANCE_NAME=myFinance
+INSTANCE_NAME=helloTest
 PORTS="-p 4848:4848 -p 8080:8080 -p 8181:8181"
-DOCKER_DIR=/tmp/DOCKER_MYFINANCE
+DOCKER_DIR=/tmp/DOCKER_HELLO
 DEPLOY_DIR=$DOCKER_DIR/deployments
 SCRIPTS_DIR=$DOCKER_DIR/scripts
 # Commands
 rm -Rf $DOCKER_DIR
 mkdir -p $DEPLOY_DIR
 mkdir -p $SCRIPTS_DIR
-cp ./target/myFinance.war          $DEPLOY_DIR
-cp postboot.txt                    $SCRIPTS_DIR
-cp ~/.m2/repository/mysql/mysql-connector-java/8.0.21/mysql-connector-java-8.0.21.jar $SCRIPTS_DIR
+cp ./target/Hello.war          $DEPLOY_DIR
 docker stop $INSTANCE_NAME || true
 docker rm   $INSTANCE_NAME || true
-docker run --name $INSTANCE_NAME --rm -d $PORTS -v $DEPLOY_DIR:/opt/payara/deployments -v $SCRIPTS_DIR:/tmp -e POSTBOOT_COMMANDS=/tmp/postboot.txt $IMAGE_NAME
+docker run --name $INSTANCE_NAME --rm -d $PORTS -v $DEPLOY_DIR:/opt/payara/deployments $IMAGE_NAME
+
